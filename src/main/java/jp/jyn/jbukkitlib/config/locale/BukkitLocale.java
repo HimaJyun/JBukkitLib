e4eb69bade6491de0cafca3be40c2e8bddf8cb73
HimaJyun <HimaJyun@users.noreply.github.com>
2021-01-03 04:15:24 +0900

    multiple player method

--- src/main/java/jp/jyn/jbukkitlib/config/locale/BukkitLocale.java
+++ src/main/java/jp/jyn/jbukkitlib/config/locale/BukkitLocale.java
@@ -3,6 +3,12 @@ package jp.jyn.jbukkitlib.config.locale;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -49,6 +55,116 @@ public interface BukkitLocale<T> extends Iterable<T> {
      * @return default locale
      */
     T get();
+
+    /**
+     * Get objects for specified players locale.
+     *
+     * @param players target players
+     * @return locale object mapping
+     */
+    default Map<T, List<Player>> get(Player... players) {
+        switch (players.length) {
+            case 0:
+                return Collections.emptyMap();
+            case 1:
+                return Collections.singletonMap(get(players[0]), Collections.singletonList(players[0]));
+        }
+
+        Map<T, List<Player>> r = new IdentityHashMap<>();
+        for (Player player : players) {
+            T obj = get(player);
+            r.computeIfAbsent(obj, ign -> new ArrayList<>()).add(player);
+        }
+        return r;
+    }
+
+    /**
+     * Get objects for specified senders locale.
+     *
+     * @param senders target senders
+     * @return locale object mapping
+     */
+    default Map<T, List<CommandSender>> get(CommandSender... senders) {
+        switch (senders.length) {
+            case 0:
+                return Collections.emptyMap();
+            case 1:
+                return Collections.singletonMap(get(senders[0]), Collections.singletonList(senders[0]));
+        }
+
+        Map<T, List<CommandSender>> r = new IdentityHashMap<>();
+        for (CommandSender sender : senders) {
+            T obj = get(sender);
+            r.computeIfAbsent(obj, ign -> new ArrayList<>()).add(sender);
+        }
+        return r;
+    }
+
+    /**
+     * <p>Get objects for specified senders locale.</p>
+     * <p>This method is an alias for {@link #getSenders(Collection)} for consistency in our API's.
+     * If you need the {@code Map<T, List<Player>>} object, use {@link #getPlayers(Collection)}.</p>
+     *
+     * @param senders target senders
+     * @return locale object mapping
+     */
+    default Map<T, List<CommandSender>> get(Collection<? extends CommandSender> senders) {
+        return getSenders(senders);
+    }
+
+    /**
+     * Get objects for specified players locale.
+     *
+     * @param players target players
+     * @return locale object mapping
+     */
+    default Map<T, List<Player>> getPlayers(Collection<Player> players) {
+        if (players.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        Iterator<Player> i = players.iterator();
+        //noinspection ResultOfMethodCallIgnored
+        i.hasNext();
+        Player first = i.next();
+        if (!i.hasNext()) {
+            return Collections.singletonMap(get(first), Collections.singletonList(first));
+        }
+
+        Map<T, List<Player>> r = new IdentityHashMap<>();
+        do {
+            Player player = i.next();
+            r.computeIfAbsent(get(player), ign -> new ArrayList<>()).add(player);
+        } while (i.hasNext());
+        return r;
+    }
+
+    /**
+     * Get objects for specified senders locale.
+     *
+     * @param senders target senders
+     * @return locale object mapping
+     */
+    default Map<T, List<CommandSender>> getSenders(Collection<? extends CommandSender> senders) {
+        if (senders.isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        Iterator<? extends CommandSender> i = senders.iterator();
+        //noinspection ResultOfMethodCallIgnored
+        i.hasNext();
+        CommandSender first = i.next();
+        if (!i.hasNext()) {
+            return Collections.singletonMap(get(first), Collections.singletonList(first));
+        }
+
+        Map<T, List<CommandSender>> r = new IdentityHashMap<>();
+        do {
+            CommandSender sender = i.next();
+            r.computeIfAbsent(get(sender), ign -> new ArrayList<>()).add(sender);
+        } while (i.hasNext());
+        return r;
+    }
 
     /**
      * Iterate all locales.
