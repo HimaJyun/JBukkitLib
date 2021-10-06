b5a0c3e432194c94170766db13e21688c4bcf128
HimaJyun <HimaJyun@users.noreply.github.com>
2021-10-06 22:17:27 +0900

    Delete old utils

--- src/test/java/jp/jyn/jbukkitlib/config/parser/ExpressionParserTest.java
+++ src/test/java/jp/jyn/jbukkitlib/config/parser/ExpressionParserTest.java
@@ -1,6 +1,5 @@
 package jp.jyn.jbukkitlib.config.parser;
 
-import jp.jyn.jbukkitlib.util.MapBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -88,14 +87,14 @@ public class ExpressionParserTest {
         assertEquals(Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) + Math.pow(z1 - z2, 2)), p.calc(), 0);
         assertEquals(1 + Math.min(1, Math.abs(-(5 * 10))), new ExpressionParser("1+min(1,abs(negate(5*10)))").calc(), 0);
 
-        Map<String, Double> variable = MapBuilder.initMap(new HashMap<>(), m -> {
-            m.put("x1", (double) x1);
-            m.put("x2", (double) x2);
-            m.put("y1", (double) y1);
-            m.put("y2", (double) y2);
-            m.put("z1", (double) z1);
-            m.put("z2", (double) z2);
-        });
+        Map<String, Double> variable = Map.of(
+            "x1", (double) x1,
+            "x2", (double) x2,
+            "y1", (double) y1,
+            "y2", (double) y2,
+            "z1", (double) z1,
+            "z2", (double) z2
+        );
         assertEquals(new ExpressionParser("sqrt(pow(x1-x2,2)+pow(y1-y2,2)+pow(z1-z2,2))").calc(variable), p.calc(), 0);
     }
 }
