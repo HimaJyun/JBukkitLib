e6a1375498dba3f389cef8504ebd2118b1f33ede
HimaJyun <HimaJyun@users.noreply.github.com>
2021-10-06 22:20:40 +0900

    Support float point and negative value.

--- src/main/java/jp/jyn/jbukkitlib/config/parser/TimeParser.java
+++ src/main/java/jp/jyn/jbukkitlib/config/parser/TimeParser.java
@@ -1,6 +1,6 @@
 package jp.jyn.jbukkitlib.config.parser;
 
-import java.util.Map;
+import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,16 +16,51 @@ import java.util.concurrent.TimeUnit;
  * </ul>
  */
 public class TimeParser {
-    private final static Map<Character, TimeUnit> UNIT_TABLE = Map.of(
-        'd', TimeUnit.DAYS,
-        'D', TimeUnit.DAYS,
-        'h', TimeUnit.HOURS,
-        'H', TimeUnit.HOURS,
-        'm', TimeUnit.MINUTES,
-        'M', TimeUnit.MINUTES,
-        's', TimeUnit.SECONDS,
-        'S', TimeUnit.SECONDS
-    );
+    /**
+     * Parses the time represented by a string.
+     *
+     * @param value       input string
+     * @param unit        Unit of returned value
+     * @param defaultUnit if unit is not specified, use this unit. (nullable, throw NumberFormatException if null)
+     * @return Parsed time
+     * @throws NumberFormatException input value can not parse.
+     */
+    public static long parse(CharSequence value, TimeUnit unit, TimeUnit defaultUnit) {
+        var sb = new StringBuilder();
+        BigDecimal result = BigDecimal.ZERO;
+
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            TimeUnit u = switch (c) {
+                case 'd', 'D', '日' -> TimeUnit.DAYS;
+                case 'h', 'H', '時' -> TimeUnit.HOURS;
+                case 'm', 'M', '分' -> TimeUnit.MINUTES;
+                case 's', 'S', '秒' -> TimeUnit.SECONDS;
+                case '㎳' -> TimeUnit.MILLISECONDS; // 隠し機能
+                case 'u', '㎲', '\u03bc'/*μ*/, '\u00b5'/*µ*/ -> TimeUnit.MICROSECONDS; // 隠し機能
+                case 'n', '㎱' -> TimeUnit.NANOSECONDS; // 隠し機能
+                default -> null;
+            };
+
+            if (u == null) {
+                sb.append(c);
+            } else {
+                result = result.add(parseTime(sb.toString(), unit, u));
+                sb.setLength(0);
+            }
+        }
+
+        if (!sb.isEmpty()) {
+            if (defaultUnit == null) {
+                throw new NumberFormatException(sb.toString() + " is invalid value, need unit.");
+            } else {
+                // 単位指定がなければデフォルト単位を使う
+                result = result.add(parseTime(sb.toString(), unit, defaultUnit));
+            }
+        }
+
+        return result.longValue();
+    }
 
     /**
      * Parses the time represented by a string.
@@ -33,31 +68,22 @@ public class TimeParser {
      * @param value input string
      * @param unit  Unit of returned value
      * @return Parsed time
-     * @throws IllegalArgumentException When a string that cannot be parsed is passed.
+     * @throws NumberFormatException input value can not parse.
      */
     public static long parse(CharSequence value, TimeUnit unit) {
-        long result = 0;
+        return parse(value, unit, TimeUnit.SECONDS);
+    }
 
-        int tmp = 0;
-        for (int i = 0; i < value.length(); i++) {
-            char c = value.charAt(i);
-            int v = Character.digit(c, 10);
-            // number
-            if (v != -1) {
-                tmp *= 10;
-                tmp += v;
-                continue;
-            }
-
-            // not number
-            TimeUnit u = UNIT_TABLE.get(c);
-            if (u == null) {
-                throw new IllegalArgumentException(value.toString() + " is invalid syntax.");
-            }
-            result += unit.convert(tmp, u);
-            tmp = 0;
+    private static BigDecimal parseTime(String value, TimeUnit from, TimeUnit to) {
+        // 0.5h -> 0.5 * TimeUnit.HOURS.toSeconds(1)
+        BigDecimal d;
+        try {
+            d = new BigDecimal(value);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(value + " is invalid value");
         }
 
-        return result;
+        var unit1 = BigDecimal.valueOf(from.convert(1, to));
+        return d.multiply(unit1);
     }
 }
--- src/test/java/jp/jyn/jbukkitlib/config/parser/TimeParserTest.java
+++ src/test/java/jp/jyn/jbukkitlib/config/parser/TimeParserTest.java
@@ -27,12 +27,79 @@ class TimeParserTest {
             Tests.init("12h1d", TimeUnit.SECONDS, TimeUnit.DAYS.toSeconds(1) + TimeUnit.HOURS.toSeconds(12)),
             Tests.init("1d23h45m", TimeUnit.SECONDS, TimeUnit.DAYS.toSeconds(1) + TimeUnit.HOURS.toSeconds(23) + TimeUnit.MINUTES.toSeconds(45)),
             Tests.init("1d1d", TimeUnit.SECONDS, TimeUnit.DAYS.toSeconds(2))
-        ).forEach(t -> assertEquals(TimeParser.parse(t.input, t.unit), t.actual));
+        ).forEach(t -> assertEquals(t.actual, TimeParser.parse(t.input, t.unit), t::toString));
+    }
 
+    @Test
+    public void exceptionTest() {
         assertThrows(
-            IllegalArgumentException.class,
+            NumberFormatException.class,
             () -> TimeParser.parse("invalid", TimeUnit.SECONDS)
         );
+    }
+
+    @Test
+    public void floatTest() {
+        Stream.of(
+            Tests.init("0.5h", TimeUnit.SECONDS, 1800),
+
+            Tests.init("0.5d0.5h", TimeUnit.MINUTES, 720 + 30),
+            Tests.init("0.5h0.5d", TimeUnit.MINUTES, 30 + 720),
+
+            Tests.init("0.5m1s", TimeUnit.SECONDS, 31),
+            Tests.init("1s0.5m", TimeUnit.SECONDS, 31)
+        ).forEach(t -> assertEquals(t.actual, TimeParser.parse(t.input, t.unit), t::toString));
+    }
+
+    @Test
+    public void calcTest() {
+        Stream.of(
+            Tests.init("-1h", TimeUnit.HOURS, -1),
+            Tests.init("+1h", TimeUnit.HOURS, 1),
+
+            Tests.init("+1h-1h", TimeUnit.HOURS, 0),
+            Tests.init("+1h+1h", TimeUnit.HOURS, 2),
+            Tests.init("+1h+1h+1h", TimeUnit.HOURS, 3),
+            Tests.init("-1h-1h", TimeUnit.HOURS, -2),
+            Tests.init("-1h-1h-1h", TimeUnit.HOURS, -3),
+
+            Tests.init("1h+30m", TimeUnit.MINUTES, 90),
+            Tests.init("1h-30m", TimeUnit.MINUTES, 30),
+
+            Tests.init("0.5h+30m", TimeUnit.MINUTES, 60),
+            Tests.init("0.5h-30m", TimeUnit.MINUTES, 0),
+
+            Tests.init("0.5d+0.5h", TimeUnit.MINUTES, 750),
+            Tests.init("0.5d-0.5h", TimeUnit.MINUTES, 690)
+        ).forEach(t -> assertEquals(t.actual, TimeParser.parse(t.input, t.unit), t::toString));
+    }
+
+    @Test
+    public void emptyTest() {
+        assertEquals(30, TimeParser.parse("30", TimeUnit.SECONDS));
+        assertEquals(90, TimeParser.parse("1m30", TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void secretUnitTest() {
+        Stream.of(
+            Tests.init("1日", TimeUnit.SECONDS, TimeUnit.DAYS.toSeconds(1)),
+            Tests.init("1時", TimeUnit.SECONDS, TimeUnit.HOURS.toSeconds(1)),
+            Tests.init("1分", TimeUnit.SECONDS, TimeUnit.MINUTES.toSeconds(1)),
+            Tests.init("1秒", TimeUnit.SECONDS, TimeUnit.SECONDS.toSeconds(1)),
+
+            Tests.init("1u", TimeUnit.NANOSECONDS, TimeUnit.MICROSECONDS.toNanos(1)),
+            Tests.init("1n", TimeUnit.NANOSECONDS, TimeUnit.NANOSECONDS.toNanos(1)),
+
+            Tests.init("1㎳", TimeUnit.NANOSECONDS, TimeUnit.MILLISECONDS.toNanos(1)),
+            Tests.init("1㎲", TimeUnit.NANOSECONDS, TimeUnit.MICROSECONDS.toNanos(1)),
+            Tests.init("1㎱", TimeUnit.NANOSECONDS, TimeUnit.NANOSECONDS.toNanos(1)),
+
+            Tests.init("1\u03bc", TimeUnit.MICROSECONDS, TimeUnit.NANOSECONDS.toNanos(1)),
+            Tests.init("1μ", TimeUnit.MICROSECONDS, TimeUnit.NANOSECONDS.toNanos(1)),
+            Tests.init("1\u00b5", TimeUnit.MICROSECONDS, TimeUnit.NANOSECONDS.toNanos(1)),
+            Tests.init("1µ", TimeUnit.MICROSECONDS, TimeUnit.NANOSECONDS.toNanos(1))
+        ).forEach(t -> assertEquals(t.actual, TimeParser.parse(t.input, t.unit), t::toString));
     }
 
     private final static class Tests {
@@ -48,6 +115,11 @@ class TimeParserTest {
 
         private static Tests init(String input, TimeUnit unit, long actual) {
             return new Tests(input, unit, actual);
+        }
+
+        @Override
+        public String toString() {
+            return input + " " + unit;
         }
     }
 }
